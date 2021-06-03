f48ff964a05adff1b76d570b10329add6ba1ce49
dnamingha <80352168+dnamingha@users.noreply.github.com>
2021-06-03 20:08:47 +0200

    JS Day Details
    Included last updated time of weather report

--- src/script.js
+++ src/script.js
@@ -1,14 +1,49 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#main-temperature");
   let cityElement = document.querySelector("#city");
   let weatherDescription = document.querySelector("#weather-description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dayElement = document.querySelector("#day-details");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   weatherDescription.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dayElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "3dabf398aa6a2086da228af67b9eb50c";
