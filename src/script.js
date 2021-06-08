4480ae8091e1a0bf4ed5b63f977bc581fcfd0d85
dnamingha <80352168+dnamingha@users.noreply.github.com>
2021-06-08 21:27:06 +0200

    Transferred forecast HTML into JS
    First forecast JS skeleton

--- src/script.js
+++ src/script.js
@@ -22,6 +22,40 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast(response) {
+  console.log(response.data.daily);
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Tue", "Wed", "Thu", "Fri"];
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function(day) {
+    forecastHTML = forecastHTML +
+    `
+      <div class="col-2">
+        <div class="weather-forecast-day">
+          ${day}
+        </div>
+        <img
+          src="https://openweathermap.org/img/wn/01d@2x.png"
+          alt=""
+          width="36">
+        <div class="weather-forecast-temperature">
+          <span class="weather-forecast-temperature-max">18°</span> | <span class="weather-forecast-temperature-min">12°</span>
+        </div>
+      </div>
+    `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML  = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "3dabf398aa6a2086da228af67b9eb50c";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#main-temperature");
   let cityElement = document.querySelector("#city");
@@ -37,9 +71,16 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dayElement.innerHTML = formatDate(response.data.dt * 1000);
-  iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-  iconElement.setAttribute("alt", `${response.data.weather[0].description}`);
-}
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  iconElement.setAttribute(
+    "alt",
+    `${response.data.weather[0].description}`
+    );
+  getForecast(response.data.coord);
+  }
 
 function search(city) {
   let apiKey = "3dabf398aa6a2086da228af67b9eb50c";
